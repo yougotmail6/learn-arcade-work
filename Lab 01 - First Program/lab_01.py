34b0ec378d06435a4629555b739a424450c6957e
yougotmail6 <bengalsfan141855@gmail.com>
2023-01-16 18:10:59 -0500

    Update lab_01.py

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,6 @@
+print("This is not the start of the actual program")
+print("\tThis is the actual start of the script")
+print("I actually believe that I may have gotten the beginning code wrong")
+print("\"Many have tried to decode this, but have you?\"")
+print("00000111100000101010101000000")
+
