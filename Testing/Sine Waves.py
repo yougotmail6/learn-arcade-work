20cc5d0c5bb742eb5bc0b75cd2113a1fd16f0b38
yougotmail6 <bengalsfan141855@gmail.com>
2023-01-26 11:59:02 -0500

    d

---
+++ Testing/Sine Waves.py
@@ -0,0 +1,61 @@
+import arcade
+import random
+import math
+
+
+arcade.open_window(600, 600, "Sine Waves")
+arcade.set_background_color(arcade.csscolor.BLACK)
+arcade.set_background_color((0, 0, 0))
+arcade.start_render()
+
+
+
+for i in range(500):
+    arcade.draw_point(i/.4+0,math.sin(math.radians(i)) * 100 + 400,arcade.csscolor.RED,4)
+    arcade.draw_point(i / .4 + 0, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 20, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 40, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 60, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 80, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 100, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.DARK_RED, 4)
+    arcade.draw_point(i / .4 + 120, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 + 140, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BROWN, 4)
+    arcade.draw_point(i / .4 + 160, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.ROSY_BROWN, 4)
+    arcade.draw_point(i / .4 + 180, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 200, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 220, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 240, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 260, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 280, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 300, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 320, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 340, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 360, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 380, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 + 400, math.sin(math.radians(i)) * 100 + 400, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i/.4+0,math.sin(math.radians(i)) * 100 + 400,arcade.csscolor.RED,4)
+    arcade.draw_point(i / .4 - 0, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 20, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 40, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 60, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 80, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 100, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.DARK_RED, 4)
+    arcade.draw_point(i / .4 - 120, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.RED, 4)
+    arcade.draw_point(i / .4 - 140, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BROWN, 4)
+    arcade.draw_point(i / .4 - 160, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.ROSY_BROWN, 4)
+    arcade.draw_point(i / .4 - 180, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 200, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 220, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 240, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 260, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 280, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 300, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 320, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 340, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 360, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 380, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+    arcade.draw_point(i / .4 - 400, math.sin(math.radians(i)) * 100 + 200, arcade.csscolor.BLUE, 4)
+
+
+arcade.finish_render()
+arcade.run()
---
+++ Testing/Testing 2.py
@@ -0,0 +1,17 @@
+PI = 3.14159269
+
+awnserA = True
+anwserB = False
+
+print(awnserA or anwserB)
+print(awnserA and anwserB)
+print(not awnserA)
+
+anwserC = 10
+awnserD = 20
+
+print((anwserC < awnserD))
+
+anwserC *= awnserD
+print(anwserC)
+
