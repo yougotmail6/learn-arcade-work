3c1f39b518e4b0351c23ab66c321b6e99d44d71d
yougotmail6 <bengalsfan141855@gmail.com>
2023-04-05 10:21:45 -0400

    Good Luck

--- Lab 09 - Sprites and Walls/lab_09.py
+++ Lab 09 - Sprites and Walls/lab_09.py
@@ -5,14 +5,16 @@ Artwork from https://kenney.nl
 
 If Python and Arcade are installed, this example can be run from the command line with:
 python -m arcade.examples.sprite_move_scrolling
+
+Your Computer May have isssues running this
 """
 
 import random
 import arcade
 from pyglet.math import Vec2
-
 SPRITE_SCALING_BOX = .7
 SPRITE_BARRACK_BOX = .7
+SPRITE_COIN_SCALING_BOX = 1
 
 DEFAULT_SCREEN_WIDTH = 800
 DEFAULT_SCREEN_HEIGHT = 600
@@ -41,10 +43,12 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = None
         self.wall_list = None
-        self.barrack_list = None
+        self.coin_list = None
+
 
         # Set up the player
         self.player_sprite = None
+        self.score = 0
 
         # Physics engine so we don't run into walls.
         self.physics_engine = None
@@ -71,13 +75,17 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
-        self.barrack_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        self.score = 0
+        self.good_music = arcade.load_sound("Bell Ding Sound Effect (download).wav")
+
 
         # Set up the player
         self.player_sprite = arcade.Sprite("scifiUnit_09.png",
                                            scale=.8)
         self.player_sprite.center_x = 256
-        self.player_sprite.center_y = 350
+        self.player_sprite.center_y = 390
         self.player_sprite.change_x = 1
         self.player_sprite.change_y = 1
         self.player_list.append(self.player_sprite)
@@ -86,7 +94,7 @@ class MyGame(arcade.Window):
 
         # --- Place walls with a list
         # Place boxes inside a loop
-        for x in range(100, 15500, 32):
+        for x in range(100, 2000, 32):
             wall = arcade.Sprite("scifiTile_41.png", SPRITE_SCALING_BOX)
             wall.center_x = x
             wall.center_y = 350
@@ -95,13 +103,51 @@ class MyGame(arcade.Window):
 
         # --- Place walls with a list
 
-        for x in range(100, 15500, random.randrange(64,600)):
+        for x in range(100, 2000, 300):
             barracks = arcade.Sprite("scifiStructure_03.png", SPRITE_BARRACK_BOX)
             barracks.center_x = x
             barracks.center_y = 390
             self.wall_list.append(barracks)
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
+        for x in range(100, 2000, 32):
+            top_floor = arcade.Sprite("scifiTile_41.png", SPRITE_SCALING_BOX)
+            top_floor.center_x = x
+            top_floor.center_y = 475
+            self.wall_list.append(top_floor)
+
+        for y in range(390,500, 32):
+            end_barrier = arcade.Sprite("scifiTile_41.png", SPRITE_SCALING_BOX)
+            end_barrier.center_x = 1980
+            end_barrier.center_y = y
+            self.wall_list.append(end_barrier)
+
+        # --- Place walls with a list
+        coordinate_list = [[300, 420],
+                           [600, 420],
+                           [1000, 420],
+                           [1100, 420],
+                           [450, 420],
+                           [545, 380],
+                           [780, 420],
+                           [860, 420],
+                           [940, 420],
+                           [1350, 380],
+                           [630, 380],
+                           [1400, 420],
+                           [1540, 380],
+                           [1700, 420],
+                           [800, 380],
+                           [750, 380],
+                           [900, 380],
+                           [950,380]]
+
+        # Loop through coordinates
+        for coordinate in coordinate_list:
+            coin = arcade.Sprite("tile_0029.png", SPRITE_COIN_SCALING_BOX)
+            coin.center_x = coordinate[0]
+            coin.center_y = coordinate[1]
+            self.coin_list.append(coin)
 
 
 
@@ -123,7 +169,8 @@ class MyGame(arcade.Window):
         # Draw all the sprites.
         self.wall_list.draw()
         self.player_list.draw()
-        self.barrack_list.draw()
+        self.coin_list.draw()
+
 
 
         # Select the (unscrolled) camera for our GUI
@@ -140,6 +187,13 @@ class MyGame(arcade.Window):
         text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, " \
                f"{self.camera_sprites.position[1]:5.1f})"
         arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
+
+        arcade.draw_text("The Left arrow key does not work, it does not work for a reason", 0, 50, (arcade.color.BLACK_BEAN), 16)
+
+
+        arcade.draw_rectangle_filled(10,508,200,40,arcade.color.ALMOND,0)
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 500, arcade.color.BLACK_BEAN, 14)
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed. """
@@ -236,7 +290,24 @@ class MyGame(arcade.Window):
 
         self.camera_gui.resize(int(width), int(height))
 
+    def update(self, delta_time):
+        """ Movement and game logic """
 
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        self.coin_list.update()
+
+
+        # Generate a list of all sprites that collided with the player.
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.coin_list)
+
+
+        # Loop through each colliding sprite, remove it, and add to the score.
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+            self.good_music.play()
 
 def main():
     """ Main function """
